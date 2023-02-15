1d43450c38dcf1dd304f4c4a9942fbd30f82eee6
KatisoTheGreat <katisoledwaba@gmail.com>
2023-02-15 10:46:38 +0200

    added mongoDB to save messages to DB

--- server.js
+++ server.js
@@ -3,29 +3,42 @@ var bodyParser = require('body-parser');
 var app = express();
 var http  = require('http').Server(app);
 var io = require('socket.io')(http);
+var mongoose = require('mongoose');
 
 app.use(express.static(__dirname));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
-var messages = [
-    {name: 'Tim', message: 'Hi'},
-    {name: 'Jane', message: 'Hello'}
-]
+var dbURL = 'mongodb+srv://katiso:monalisa@cluster0.bnu1w2y.mongodb.net/?retryWrites=true&w=majority';
+
+var Message = mongoose.model('Message', {
+    name: String,
+    message: String
+})
 
 app.get('/messages', (req, res) => {
-    res.send(messages);
+    Message.find({}, (err, messages) => {
+        res.send(messages);
+    })
 })
 
 app.post('/messages', (req, res) => {
-    messages.push(req.body);
-    io.emit('message', req.body);
-    res.sendStatus(200);
+    var message = new Message(req.body);
+    message.save((err) => {
+        if(err)
+            sendStatus(500);
+        io.emit('message', req.body);
+        res.sendStatus(200);
+    });
 })
 
 io.on('connection', (socket) => {
     console.log('A user connected');
 })
+
+mongoose.connect(dbURL, (err) => {
+    console.log('MongoDB connection', err);
+})  
 
 var server = http.listen(3000, () => {
     console.log('Server is listening on port', server.address().port);
